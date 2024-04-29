9c29d3757f6c54021f81768b95e98ee991ca8072
jamalt23 <jamalt.live@gmail.com>
2024-04-29 18:05:05 +0400

    code optimized

--- password_game.py
+++ password_game.py
@@ -7,129 +7,130 @@ clear_terminal = lambda: system('clear')
 clear_clipboard = lambda: pyperclip.copy('')
 
 print("Original game: neal.fun/password-game")
-current_year = str(datetime.now().year)
-a="0";languages=[["english","eng"],["russian","ru"]];game = True
-days=["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье","monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+game = True
+days = ["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье",
+        "monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 lang=(input("Select language(Russian/English): ")).lower()
 if lang!='quit':
-    while (not lang in languages[0]) and (not lang in languages[1]):
+    while not lang in ('russian', 'ru', 'english', 'en'):
         print("Invalid: You are Invalid")
         lang=(input("Select language(Russian/English): ")).lower()
 else:
     print('Good bye!')
     quit()
-if lang in languages[0]:
-    length="Length";ent_password="Enter your password: ";ent_name="Enter your name: "
-    signup="Sign up";login="Log in";lose="Your password is wrong.";gameover="Game over.";print(signup)
-    name = input(ent_name);win=f"Successful! Welcome, {name}.\nYou won!"
-    rule1="Your password must be at least 8 characters"
-    rule2="Your password must include a number"
-    rule3="Your password must include an uppercase letter"
-    rule4="Your password must include a special character"
-    rule5="The digits in your password must add up to 40" 
-    rule6="Your password must include a day of the week"
-    rule7="Your password must include current year"
-    rule8="Your password must include the length of your password"
-    rule9="Your password must include the key🔑 emoji"
-    rule10="Your password must include your name"
-    rule11="The length of your password must be even number"
-    rule12="Your password must include the current time backwards (Example: 01:02 -> 20:10)"
-    confirm="Do you confirm that this is your password?(Yes or Yes) "
-elif lang in languages[1]:
-    length="Длина";ent_password="Введите пароль: ";ent_name="Введите ваше имя: "
-    signup="Регистрация";login="Вход";lose="Неверный пароль.";gameover="Игра окончена.";print(signup)
-    name = input(ent_name);win=f"Успешно! Добро пожаловать, {name}.\nВы победили!"
-    rule1="Ваш пароль должен быть не менее 8 символов в длину"
-    rule2="Ваш пароль должен содержать хоть одну цифру"
-    rule3="Ваш пароль должен содержать заглавную букву"
-    rule4="Ваш пароль должен содержать любой знак пунктуации"
-    rule5="Сумма цифр в вашем пароле должна быть 40"
-    rule6="Ваш пароль должен содержать день недели"
-    rule7="Ваш пароль должен содержать текущий год"
-    rule8="Ваш пароль должен содержать длину вашего пароля"
-    rule9="Ваш пароль должен содержать эмодзи ключа🔑"
-    rule10="Ваш пароль должен содержать ваше имя"
-    rule11="Длина вашего пароля должна быть чётным числом"
-    rule12="Ваш пароль должен содержать текущее время наоборот (Пример: 01:02 -> 20:10)"
-    confirm="Вы подтверждаете что это ваш пароль?(Да или Да) "
-a = input(ent_password)
+
+MESSAGES_EN = {
+    'length': 'Length', 'ent_password': 'Enter your password: ', 'ent_name': 'Enter your name: ',
+    'signup': 'Sign up', 'login': 'Log in', 'lose': 'Your password is wrong.', 'gameover': 'Game over.',
+    'confirm': 'Do you confirm that this is your password?(Yes or Yes) ',
+    'rule1': 'Your password must be at least 8 characters',
+    'rule2': 'Your password must include a number',
+    'rule3': 'Your password must include an uppercase letter',
+    'rule4': 'Your password must include a special character',
+    'rule5': 'The digits in your password must add up to 40',
+    'rule6': 'Your password must include a day of the week',
+    'rule7': 'Your password must include current year',
+    'rule8': 'Your password must include the length of your password',
+    'rule9': 'Your password must include the key🔑 emoji',
+    'rule10': 'Your password must include your name',
+    'rule11': 'The length of your password must be even number',
+    'rule12': 'Your password must include the current time backwards (Example: 01:02 -> 20:10)',
+    'win': 'Successful! Welcome, {}.\nYou won!'
+}
+MESSAGES_RU = {
+    'length': 'Длина', 'ent_password': 'Введите пароль: ', 'ent_name': 'Введите ваше имя: ',
+    'signup': 'Регистрация', 'login': 'Вход', 'lose': 'Неверный пароль.', 'gameover': 'Игра окончена.',
+    'confirm': 'Вы подтверждаете что это ваш пароль?(Да или Да) ',
+    'rule1': 'Ваш пароль должен быть не менее 8 символов в длину',
+    'rule2': 'Ваш пароль должен содержать хоть одну цифру',
+    'rule3': 'Ваш пароль должен содержать заглавную букву',
+    'rule4': 'Ваш пароль должен содержать любой знак пунктуации',
+    'rule5': 'Сумма цифр в вашем пароле должна быть 40',
+    'rule6': 'Ваш пароль должен содержать день недели',
+    'rule7': 'Ваш пароль должен содержать текущий год',
+    'rule8': 'Ваш пароль должен содержать длину вашего пароля',
+    'rule9': 'Ваш пароль должен содержать эмодзи ключа🔑',
+    'rule10': 'Ваш пароль должен содержать ваше имя',
+    'rule11': 'Длина вашего пароля должна быть чётным числом',
+    'rule12': 'Ваш пароль должен содержать текущее время обратное (Пример: 01:02 -> 20:10)',
+    'win': 'Успешно! Добро пожаловать, {}.\nВы победили!'
+}
+
+if lang in ("english","en"):
+    messages = MESSAGES_EN
+elif lang in ("russian","ru"):
+    messages = MESSAGES_RU
+
+print(messages['signup'])
+name = input(messages['ent_name'])
+messages['win'] = messages['win'].format(name)
+
+#Monday2024001Jamal028!🔑95:71
 
 contain_digit = lambda x: any(char.isdigit() for char in x)
 contain_upper = lambda x: any(char.isupper() for char in x)
 contain_punctuation = lambda x: any(char in punctuation for char in x)
-digits_summ = lambda x: sum(int(char) for char in x)
-contain_day = lambda x: any(day in x for day in days)
-contain_year = lambda x: current_year in x
+digits_summ = lambda x: sum(int(char) for char in x if char.isdigit())
+contain_day = lambda x: any(day.lower() in x.lower() for day in days)
+contain_year = lambda x: str(datetime.now().year) in x
 even_length = lambda x: len(x)%2==0
 contain_time = lambda x: datetime.now().strftime("%H:%M")[::-1] in x
 
-#Monday2024001Jamal028!🔑95:71
+class Password:
+    def __init__(self: object, password: str):
+        self.password = password
+        self.length = len(password)
+        self.length_rule = len(password) >= 8 # rule 1
+        self.contain_digit = contain_digit(password) # rule 2
+        self.contain_upper = contain_upper(password) # rule 3
+        self.contain_punctuation = contain_punctuation(password) # rule 4
+        self.digits_summ = digits_summ(password) # rule 5
+        self.contain_day = contain_day(password) # rule 6
+        self.contain_year = contain_year(password) # rule 7
+        self.contain_key = '🔑' in password # rule 9
+        self.contain_name = name in password # rule 10
+        self.even_length = even_length(password) # rule 11
+        self.contain_time = contain_time(password) # rule 12
+        self.rules = [self.length_rule, self.contain_digit, self.contain_upper, self.contain_punctuation,
+                      self.digits_summ, self.contain_day, self.contain_year, self.contain_key,
+                      self.contain_name, self.even_length, self.contain_time]
 
-while game:
-    pyperclip.copy(a)
-    print(a) if a!="quit" else print("Game over.")
-    if a=='quit': print('Game over.');quit()
-    elif len(a)<8:
-        print(f"{length}: {len(a)}")
-        print(rule1)
-        a = input(ent_password)
-    elif not contain_digit(a): 
-        print(f"{length}: {len(a)}")
-        print(rule2)
-        a = input(ent_password)
-    elif not contain_upper(a): 
-        print(f"{length}: {len(a)}")
-        print(rule3)
-        a = input(ent_password)
-    elif not contain_punctuation(a):
-        print(f"{length}: {len(a)}")
-        print(rule4)
-        a = input(ent_password)
-    elif not digits_summ(a)!=40:
-        print(f"{length}: {len(a)}")
-        print(rule5)
-        a = input(ent_password)
-    elif not contain_day(a):
-        print(f"{length}: {len(a)}")
-        print(rule6)
-        a = input(ent_password)
-    elif not contain_year(a): 
-        print(f"{length}: {len(a)}")
-        print(rule7)
-        a = input(ent_password)
-    elif not str(len(a)) in a:
-        print(f"{length}: {len(a)}")
-        print(rule8)
-        a = input(ent_password)
-    elif not "🔑" in a:
-        print(f"{length}: {len(a)}")
-        print(rule9)
-        a = input(ent_password)
-    elif (not name in a) and (not name.lower() in a):
-        print(f"{length}: {len(a)}")
-        print(rule10)
-        a = input(ent_password)
-    elif not even_length(a):
-        print(f"{length}: {len(a)}")
-        print(rule11)
-        a = input(ent_password)
-    elif not contain_time(a):
-        print(f"{length}: {len(a)}")
-        print(rule12)
-        a = input(ent_password)  
-    else:
-        game = False
+    def __str__(self: object) -> str:
+        return self.password
+
+    def is_valid(self: object) -> bool:
+        return all(self.rules)
+
+    def check_rules(self: object) -> bool | str:
+        for rule in self.rules:
+            ruleindex = self.rules.index(rule)+1
+            # print(f"{messages[f'rule{ruleindex}']}: {rule}")
+            if not rule:
+                return messages[f'rule{ruleindex}']
+        return True
+
+password = Password(input(messages['ent_password']))
+
+while True:
+    pyperclip.copy(password.password)
+    print(password) if password!="quit" else print("Game over.")
+    if password.is_valid():
+        print(messages['win'])
         break
+    else:
+        print(password.check_rules())
+        password = Password(input(messages['ent_password']))
+
 if not game:
-    answer = (input(confirm)).lower()
-    if (answer=="yes") or (answer=="да"):
+    answer = (input(messages['confirm'])).lower()
+    if answer in ('yes', 'y', 'да'):
         clear_terminal()
         clear_clipboard()
-        print(login)
-        pw = input(ent_password)
-        if pw!=a:
-            print(lose);print(gameover)
+        print(messages['login'])
+        pw = input(messages['ent_password'])
+        if pw!=password:
+            print(messages['lose']);print(messages['gameover'])
         else:
-            print(win)
+            print(messages['win'])
     else:
-        print(gameover)
+        print(messages['gameover'])
